4760f5fa20a393f820074195b8187c7d7ad53af5
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-08-08 00:31:13 +0530

    Arrays medium level

---
+++ Arrays2/learders_in_an_array.java
@@ -0,0 +1,27 @@
+package Arrays2;
+import java.util.*;
+public class learders_in_an_array {
+	public static ArrayList<Integer> printLeadersBruteForce(int[] arr, int n){
+		ArrayList<Integer> ans=new ArrayList<>();
+		int max=arr[n-1];
+		ans.add(arr[n-1]);
+		for(int i=n-2;i>0;i--) {
+			if(arr[i]>max) {
+				ans.add(arr[i]);
+				max=arr[i];
+			}
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		int n = 6;
+		int arr[]=  {10, 22, 12, 3, 0, 6};
+		ArrayList<Integer> ans= printLeadersBruteForce(arr,n);
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(ans);
+			break;
+		}
+	}
+
+}
