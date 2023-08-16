08a9b4b39845bf3731cb853ecc6dbb11ff1f0d9b
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-08-16 22:57:45 +0530

    binary search

--- BS_ONE_D_ARRAYS/Search_insert_position.java
+++ BS_ONE_D_ARRAYS/Search_insert_position.java
@@ -3,7 +3,22 @@ package BS_ONE_D_ARRAYS;
 public class Search_insert_position {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubclass Solution {
+//	    public int searchInsert(int[] nums, int target) {
+//	        int start = 0;
+//	        int end = nums.length-1;
+//
+//	        while (start <= end) {
+//	            int mid = start + (end-start)/2;
+		
+//	            if (nums[mid] == target) return mid;
+//	            else if (nums[mid] > target) end = mid-1;
+//	            else start = mid+1;
+//	        }
+//
+//	        return start;
+//	    }
+//	}
 
 	}
 
