0c56703154b0d38bbfbf12e2ddc0bb70fd315261
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-08-21 14:55:09 +0530

    Strings

--- Strings2/Maximum_Nesting_Depth_of_the_Parentheses.java
+++ Strings2/Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -4,7 +4,25 @@ public class Maximum_Nesting_Depth_of_the_Parentheses{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		s = "(1+(2*3)+((8)/4))+1"
+		//3
 	}
 
 }
+//class Solution {
+//    public int maxDepth(String s) {
+//        int upcount=0;
+//        int downcount=0;
+//        int ans=0;
+//        for(int i =0; i<s.length(); i++){
+//            if(s.charAt(i)=='('){
+//                upcount++;
+//            }
+//            ans=Math.max(ans,upcount);
+//            if(s.charAt(i)==')'){
+//                upcount--;
+//            }
+//        }
+//        return ans;
+//    }
+//}
