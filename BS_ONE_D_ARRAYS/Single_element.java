47c6331185af912feaf5c2d3bf408ca12367664c
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2024-01-06 19:22:32 +0530

    binary search

--- BS_ONE_D_ARRAYS/Single_element.java
+++ BS_ONE_D_ARRAYS/Single_element.java
@@ -2,8 +2,18 @@ package BS_ONE_D_ARRAYS;
 
 public class Single_element {
 //	[1,1,2,3,3,4,4,8,8]    ans=2;
+	public static int singleElement(int[]arr) {
+		int n=arr.length;
+		int xor=0;
+		for(int i=0;i<n;i++) {
+			xor=xor^arr[i];
+		}
+		return xor;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr= {1,1,2,3,3,4,4,8,8};
+		System.out.print(singleElement(arr));
 
 	}
 
